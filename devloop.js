d76c8b7d6975d80f3b6dbed9af7949dffe010d80
Mathieu <mathieu.ancelin@gmail.com>
2016-04-26 16:27:43 +0200

    Forgot library

--- devloop.js
+++ devloop.js
@@ -1,38 +1,38 @@
 'use strict'
 
+let compileLibrary = run({
+  name: 'compileLibrary',
+  sh: './gradlew library:compileJava',
+  watch: ['library/src/main/java/**', 'library/src/main/resources/**']
+})
+
 let compileAggregator = run({
   name: 'compileAggregator',
   sh: './gradlew aggregator:compileJava',
   watch: ['aggregator/src/main/java/**', 'aggregator/src/main/resources/**']
-})
+}).dependsOn(compileLibrary)
 
 let compileBikeService = run({
   name: 'compileBikeService',
   sh: './gradlew bikes-service:compileJava',
   watch: ['bikes-service/src/main/java/**', 'bikes-service/src/main/resources/**']
-})
+}).dependsOn(compileLibrary)
 
 let compileContainerService = run({
   name: 'compileContainerService',
   sh: './gradlew aggregator:compileJava',
   watch: ['containers-service/src/main/java/**', 'containers-service/src/main/resources/**']
-})
+}).dependsOn(compileLibrary)
 
 let compileServiceProxy = run({
   name: 'compileServiceProxy',
   sh: './gradlew service-proxy:compileJava',
   watch: ['service-proxy/src/main/java/**', 'service-proxy/src/main/resources/**']
-})
-
-/*let compileDevRunner = run({
-  name: 'compileDevRunner',
-  sh: './gradlew dev-runner:compileJava',
-  watch: ['dev-runner/src/main/java/**', 'dev-runner/src/main/resources/**']
-}).dependsOn(compileAggregator, compileBikeService, compileContainerService, compileServiceProxy)*/
+}).dependsOn(compileLibrary)
 
 let server = runServer({
   httpPort,
   sh: `./gradlew dev-runner:run -q -Dport="${httpPort}"`
-}).dependsOn(compileAggregator, compileBikeService, compileContainerService, compileServiceProxy)
+}).dependsOn(compileLibrary, compileAggregator, compileBikeService, compileContainerService, compileServiceProxy)
 
 proxy(server, 8080)
